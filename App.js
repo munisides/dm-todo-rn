11f529f01e2701bcb995aa90cd4f77a3fc0ad1ba
munisides <munisides@gmail.com>
2023-01-04 17:06:13 -0500

    Adding components and styling

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import TodoList from "./Components/TodoList";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Yet another React Native Todo App!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.root}>
+      <TodoList />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  root: {
+    width: '100%',
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'white',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "white",
   },
   text: {
-    color: 'white',
+    color: "white",
   },
 });
